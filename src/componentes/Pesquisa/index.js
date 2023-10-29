5286c553aea4b36cc1efd45718646da697a257ef
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-10-29 15:29:05 -0300

    02. Requisições com Axios

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,8 @@
 import Input from '../Input'
 import styled from 'styled-components'
 import { useState } from 'react'
-import { livros } from './dadosPesquisa'
+import { getLivros } from '../../servicos/livros'
+import { useEffect } from 'react'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -47,6 +48,16 @@ const Resultado = styled.div`
 
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
+    const [livros, setLivros] = useState([])
+
+    useEffect(() => {
+        fetchLivros()
+    }, [])
+
+    async function fetchLivros() {
+        const livrosDaAPI = await getLivros()
+        setLivros(livrosDaAPI)
+    }
 
     return (
         <PesquisaContainer>
